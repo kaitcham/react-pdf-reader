a79193746cbae954521972b2697ad712ac7b0625
Munezero Christian <chamsf5@aol.com>
2022-10-14 10:18:02 +0200

    Display resume inside the modal

--- src/components/ResumeModal.js
+++ src/components/ResumeModal.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+// react pdf viewer
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+import pdfFile from '../assets/pdf/Munezero Christian Resume.pdf';
 
 const ResumeModal = ({ setShowModal }) => {
+  const [resume] = useState(pdfFile);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="modal__background">
-      <div className="modal__container">Hello world!</div>
+      <div className="modal__container">
+        {resume && (
+          <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js">
+            <Viewer fileUrl={resume} plugins={[defaultLayoutPluginInstance]} />
+          </Worker>
+        )}
+      </div>
       <div className="close__icon" onClick={() => setShowModal(false)}>
         X
       </div>
