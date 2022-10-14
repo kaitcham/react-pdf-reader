b438712a3ac548d22328af870c9b0f70525c3ba5
Munezero Christian <chamsf5@aol.com>
2022-10-14 10:36:40 +0200

    Add animations with framer motion

--- src/components/ResumeModal.js
+++ src/components/ResumeModal.js
@@ -8,24 +8,58 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
+// framer-motion
+import { motion } from 'framer-motion';
+
 import pdfFile from '../assets/pdf/Munezero Christian Resume.pdf';
+
+// background variants
+const backgroundVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+// modal variants
+const modalVariants = {
+  hidden: {
+    x: '100vw',
+  },
+  visible: {
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 const ResumeModal = ({ setShowModal }) => {
   const [resume] = useState(pdfFile);
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
-    <div className="modal__background">
-      <div className="modal__container">
+    <motion.div
+      className="modal__background"
+      variants={backgroundVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div className="modal__container" variants={modalVariants}>
         {resume && (
           <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js">
             <Viewer fileUrl={resume} plugins={[defaultLayoutPluginInstance]} />
           </Worker>
         )}
-      </div>
+      </motion.div>
       <div className="close__icon" onClick={() => setShowModal(false)}>
         X
       </div>
-    </div>
+    </motion.div>
   );
 };
 
