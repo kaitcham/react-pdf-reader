a50c66680be0cd7306639ee9caa40e24a76470f6
Munezero Christian <chamsf5@aol.com>
2022-10-14 08:58:24 +0200

    Add a button to load resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <button>Resume</button>
+    </div>
+  );
 };
 
 export default App;
